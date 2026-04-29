5cdee2abe88e1a067038c3689ca4429debff401d
Yojhan Alor <yojhanalor1@gmail.com>
2026-04-28 19:41:02 -0500

    asistenciacorregido

--- asistencias/src/main/java/com/sistema/asistencias/entity/asistencia.java
+++ asistencias/src/main/java/com/sistema/asistencias/entity/asistencia.java
@@ -1,12 +1,22 @@
 package com.sistema.asistencias.entity;
 
-import jakarta.persistence.*;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import com.sistema.asistencias.enums.EstadoAsistencia;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 @Entity
 public class asistencia {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
